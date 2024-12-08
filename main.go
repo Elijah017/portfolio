a088044b6e1805dbc8bf01eb1c6f98f48195cb39
Elijah Mullens <elmmullens@gmail.com>
2024-12-08 17:25:59 +0800

    setup home route and start templates

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/Elijah017/portfolio/internal/routes"
 )
 
 func main() {
 	if err := routes.InitRoutes(); err == nil {
 	}
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
